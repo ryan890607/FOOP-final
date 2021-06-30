f38c7d0017f233313722c27560a6f453a8a88da7
kuo2020 <b07902056@ntu.edu.tw>
2021-07-01 00:30:36 +0800

    家慶的

--- src/ironBoar/Attack.java
+++ src/ironBoar/Attack.java
@@ -18,7 +18,7 @@ public class Attack extends CyclicSequence {
     public void update() {
         if (ironBoar.isAlive()) {
             super.update();
-            if (ironBoar.getX() < ironBoar.getTarget().getX()){
+            if (ironBoar.getX() < ironBoar.getTarget().getX() + (ironBoar.getFace() == Direction.RIGHT ? 50 : -50)){
                 ironBoar.setFace(Direction.RIGHT);
                 ironBoar.getWorld().move(ironBoar, Direction.RIGHT.translate());
             } else {
