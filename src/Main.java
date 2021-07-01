b95ed47c59c2c61a6613458473eb5a0a1cd1a9a3
jason104 <43718530+jason104@users.noreply.github.com>
2021-07-01 18:47:13 +0800

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,7 @@ public class Main {
         Knight p2 = new Knight(150, new Point(300, 300+768));
         IronBoar m1 = new IronBoar(100, new Point(300, 150));
         IronBoar m2 = new IronBoar(100, new Point(500, 150));
-        StarPixie m3 = new StarPixie(50, new Point(300, 350));
+        StarPixie m3 = new StarPixie(50, new Point(300, 430));
         
         int obstacleCount = 1;
 	    Direction d = Direction.RIGHT;
@@ -57,7 +57,8 @@ public class Main {
     	    ArrayList<Obstacle2> o2 = new ArrayList<Obstacle2>();
 	    for (int i = 0; i < obstacleCount; i++) {
 		if (i == 0)
-	            o2.add(new Obstacle2("assets/obstacle/Obstacle2.png", new Point((250)%2048, (500)%1536), d));
+			;
+	            //o2.add(new Obstacle2("assets/obstacle/Obstacle2.png", new Point((250)%2048, (500)%1536), d));
 		else if (i == 1)
 	            o2.add(new Obstacle2("assets/obstacle/Obstacle2.png", new Point((445+750)%2048, (250+300)%1536), d));
 		else if (i == 2)
@@ -68,7 +69,7 @@ public class Main {
 	            d = Direction.RIGHT;
 	    }
 
-	    obstacleCount = 4;
+	    obstacleCount = 6;
 	    d = Direction.LEFT;
 	    ArrayList<Obstacle3> o3 = new ArrayList<Obstacle3>();
 	    for (int i = 0; i < obstacleCount; i++) {
@@ -80,6 +81,10 @@ public class Main {
 	            o3.add(new Obstacle3("assets/obstacle/Obstacle3.png", new Point((250)%2048, (1036)%1536), d));
 		else if (i == 3)
 	            o3.add(new Obstacle3("assets/obstacle/Obstacle3.png", new Point((400)%2048, (330)%1536), d));
+		else if (i == 4)
+	            o3.add(new Obstacle3("assets/obstacle/Obstacle3.png", new Point((100)%2048, (600)%1536), d));
+		else if (i == 5)
+	            o3.add(new Obstacle3("assets/obstacle/Obstacle3.png", new Point((0)%2048, (330)%1536), d));
 
 	        if (d == Direction.RIGHT)
 	            d = Direction.LEFT;
@@ -87,7 +92,7 @@ public class Main {
 	            d = Direction.RIGHT;
 	    }
 	
-          World world = new World("assets/background/fallguys4times.jpg", o1, o2, o3, new KnightCollisionHandler(), p1, p2, m1, m2, m3);  // model
+        World world = new World("assets/background/fallguys4times.jpg", o1, o2, o3, new KnightCollisionHandler(), p1, p2, m1, m2, m3);  // model
         LoginWorld loginWorld = new LoginWorld("assets/background/MapleStory.png");
 
         Game game = new Game(world, p1, p2);  // controller
